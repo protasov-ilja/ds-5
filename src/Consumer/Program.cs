69c60346d7d4bb6cc9743cd4e5fce337635bad04
Vasily Kuznetsov <vasily.kuznetsov@travelline.ru>
2019-03-02 13:51:13 +0300

    A Producer-Consumer example added with implementation on Redis

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        const string QUEUE_NAME = "JOBS_QUEUE";
+        const string HINTS_CHANNEL = "JOBS_QUEUE_hints";
+
+        public static void Main( string[] args )
+        {
+            string redisUrl = args.Length > 0 ? args[0] : "localhost";
+            IConnectionMultiplexer redis = ConnectionMultiplexer.Connect( redisUrl );
+
+            // subscribe to notifications
+            redis.GetSubscriber().Subscribe( HINTS_CHANNEL, delegate
+             {
+                 // process all messages in queue
+                 string msg = redis.GetDatabase().ListRightPop( QUEUE_NAME );
+                 while (msg != null)
+                 {
+                     string jobData = ParseData( msg );
+                     DoJob( jobData );
+                     msg = redis.GetDatabase().ListRightPop( QUEUE_NAME );
+                 }
+             } );
+
+            redis.GetSubscriber().Publish( HINTS_CHANNEL, "" );
+
+            Console.WriteLine( "Press Enter to exit" );
+            Console.ReadLine();
+        }
+
+        private static void DoJob( string jobData )
+        {
+            Console.WriteLine( $"Job data: {jobData}" );
+            System.Threading.Thread.Sleep(1500); // emulate loading
+        }
+
+        private static string ParseData( string msg )
+        {
+            return msg.Split( ':' )[1];
+        }
+
+
+    }
+}
---
+++ src/Producer/Program.cs
@@ -0,0 +1,35 @@
+﻿using StackExchange.Redis;
+
+namespace Producer
+{
+    internal class Program
+    {
+        const string JOB_HINTS_CHANNEL = "job_hints";
+        const string JOBS_QUEUE_NAME = "jobs_queue";
+
+        private static void Main( string[] args )
+        {
+            string redisUrl = args.Length > 0 ? args[0] : "localhost";
+            IConnectionMultiplexer redis = ConnectionMultiplexer.Connect( redisUrl );
+
+            var msg1 = BuildJobMessage( "this is the first job" );
+            SendMessage( msg1, redis );
+
+            var msg2 = BuildJobMessage( "this is the second one" );
+            SendMessage( msg2, redis );
+        }
+
+        private static void SendMessage(string message, IConnectionMultiplexer redis )
+        {
+            // put message to queue
+            redis.GetDatabase().ListLeftPush( JOBS_QUEUE_NAME, message, flags: CommandFlags.FireAndForget );
+            // and notify consumers
+            redis.GetSubscriber().Publish( JOB_HINTS_CHANNEL, "" );
+        }
+
+        private static string BuildJobMessage(string data)
+        {
+            return $"JOB:{data}";
+        }
+    }
+}
